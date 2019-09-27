1159b752bd79a24ffaac5ed4a907524bb1d617c3
Ellie Holderness <ellie.holderness@gmail.com>
2019-09-27 12:38:02 +0100

    Add hompage for overall project

--- project/urls.py
+++ project/urls.py
@@ -15,8 +15,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('todo/', include("todo.todo_urls")),
-    path('admin/', admin.site.urls),
+    path(
+        'todo/',
+        include("todo.todo_urls")
+    ),
+    path(
+        'admin/',
+        admin.site.urls
+    ),
+    path(
+        '',
+        TemplateView.as_view(
+            template_name='index.html'
+        ),
+        name='home'
+    ),
 ]
