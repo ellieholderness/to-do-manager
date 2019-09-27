4a41298448c3b2490ad524f77b05a944a9bbb5db
Ellie Holderness <ellie.holderness@gmail.com>
2019-09-27 11:42:21 +0100

    Add ToDo and Tag models

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class ToDo(models.Model):
+    title_text = models.CharField(max_length=150)
+    notes = models.CharField(max_length=500)
+
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        related_name='children',
+        related_query_name='child',
+        null=True,
+    )
+
+class Tag(models.Model):
+    todo = models.ForeignKey(
+        ToDo,
+        on_delete=models.CASCADE,
+        )
+    name = models.CharField(max_length=20)
