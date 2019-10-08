fd9c4f25676e36af63e2cbcac0fc7f32108a10b0
Ellie Holderness <ellie.holderness@owlstone.co.uk>
2019-10-08 14:17:28 +0100

    Add global stylesheet and hook it into base.html

--- project/settings.py
+++ project/settings.py
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.dirname(__file__),'static').replace('\\','/'),
+)
