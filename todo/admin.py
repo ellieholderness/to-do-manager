e2120287621a677b75172767bcfb0441a6dd9cec
Ellie Holderness <ellie.holderness@owlstone.co.uk>
2019-10-08 11:47:06 +0100

    Add admin models

--- todo/admin.py
+++ todo/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 from .models import ToDo, Tag, Category
 
-admin.site.register(ToDo)
-admin.site.register(Tag)
-admin.site.register(Category)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = ('name',)
+
+
+@admin.register(ToDo)
+class ToDoAdmin(admin.ModelAdmin):
+	list_display = (
+		'title_text',
+		'notes',
+		'do_by_date',
+		'parent',
+		'category',
+	)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+	list_display = ('name', 'todo')
